8242c67b08cd18bff753daf69ba33075958c0548
Sarawuth <sarawuth.sonnum@wdc.com>
2018-01-23 12:10:01 +0700

    Send hello

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => "Respond :" . $content
+					'text' => "Respond : Hello user"
 				];
 			
 			replyToUser($replyToken,$messages,$access_token);
